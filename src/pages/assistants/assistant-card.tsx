c7dbb282cd25d4dcf6bab87ff4596151ad40c519
NKenzhegaliyev <nkenzhegaliyev@dar.io>
2025-04-19 19:48:00 +0500

    assistants list

---
+++ src/pages/assistants/assistant-card.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AssistantCard = ({ assistant }: any) => {
+  const navigate = useNavigate();
+  return (
+    <div className="assistants__card">
+      <div
+        className="assistants__card__avatar"
+        style={{
+          backgroundImage: `url("${assistant?.photo}")`,
+        }}
+      />
+      <div className="assistants__card__name">{assistant?.name}</div>
+      <div className="assistants__card__type">{assistant?.type}</div>
+      <ul className="assistants__card__company">
+        {assistant?.duties.map((item: any) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <button
+        disabled={assistant?.isDisabled}
+        className={`${`assistants__card__btn`} ${
+          assistant?.isDisabled ? 'assistants__card__btn-disabled' : 'assistants__card__btn-active'
+        }`}
+        onClick={() => navigate('/app/onboarding')}
+      >
+        {assistant?.action}
+      </button>
+    </div>
+  );
+};
+export default AssistantCard;
---
+++ src/pages/assistants/assistants.page.tsx
@@ -0,0 +1,122 @@
+import React, { useEffect } from 'react';
+import AssistantCard from './assistant-card';
+import marat from './assets/marat.jpg';
+import aset from './assets/aset.jpg';
+import daniar from './assets/daniar.jpg';
+import indira from './assets/indira.jpg';
+import alia from './assets/alia.jpg';
+import dinara from './assets/dinara.jpg';
+import madina from './assets/madina.jpg';
+
+import './assistants.style.scss';
+
+const AssistantsPage = () => {
+  const assistants = [
+    {
+      name: 'Асет',
+      type: 'Ассистент по нормативным документам',
+      photo: aset,
+      duties: [
+        'Ответит на любой вопрос по ВНД',
+        'Предоставит точные цитаты и ссылки на источники',
+        'Распознает противоречия в документах',
+        'Поможет сформулировать обоснованные рекомендации',
+      ],
+      action: 'Начать диалог',
+      isDisabled: false,
+    },
+    {
+      name: 'Динара',
+      type: 'Кадровый ассистент',
+      photo: dinara,
+      duties: [
+        'Подскажет по отпускам и больничным',
+        'Объяснит оргструктуру',
+        'Поможет с любыми HR-процессами',
+        'Всё о людях и для людей — с заботой и точностью',
+      ],
+      action: 'В процессе',
+      isDisabled: true,
+    },
+    {
+      name: 'Марат',
+      type: 'Юридический ассистент',
+      photo: marat,
+      duties: [
+        'Проверит договор',
+        'Поможет оформить документы',
+        'Подскажет, как действовать по правилам',
+        'Твой надёжный советник по юридическим вопросам — чётко, быстро и по делу.',
+      ],
+      action: 'В процессе',
+      isDisabled: true,
+    },
+    {
+      name: 'Данияр',
+      type: 'ИТ-ассистент',
+      photo: daniar,
+      duties: [
+        'Настроит доступы и VPN',
+        'Объяснит, как работает техника',
+        'Поможет решить любую ИТ-проблему',
+        'Пиши — и проблема с техникой исчезнет.',
+      ],
+      action: 'В процессе',
+      isDisabled: true,
+    },
+    {
+      name: 'Индира',
+      type: 'Психолог-ассистент',
+      photo: indira,
+      duties: [
+        'Поддержит в стрессовых ситуациях',
+        'Поможет выговориться и найти баланс',
+        'Подскажет, куда обратиться при выгорании',
+        'Я Здесь, чтобы вы чувствовали себя лучше.',
+      ],
+      action: 'В процессе',
+      isDisabled: true,
+    },
+    {
+      name: 'Алия',
+      type: 'Бухгалтер-ассистент',
+      photo: alia,
+      duties: [
+        'Поможет оформить авансовый отчёт',
+        'Объяснит, как работают выплаты и налоги',
+        'Проверит чеки, таблицы и формы',
+        'Всё по полочкам — быстро и понятно.',
+      ],
+      action: 'В процессе',
+      isDisabled: true,
+    },
+    {
+      name: 'Мадина',
+      type: 'Финансовый ассистент',
+      photo: madina,
+      duties: [
+        'Объяснит бюджеты и расходы',
+        'Поможет с финансовыми процедурами',
+        'Разложит цифры по полочкам',
+        'Понимает финансы лучше, чем Excel — просто спроси.',
+      ],
+      action: 'В процессе',
+      isDisabled: true,
+    },
+  ];
+
+  return (
+    <div className="assistants">
+      <h1 className={'assistants__header'}>Ассистенттер көрмесі</h1>
+      <div className={'assistants__wrapper'}>
+        <div className="assistants__grid">
+          {assistants.map((assistant, index) => (
+            <AssistantCard key={index} assistant={assistant} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AssistantsPage;
